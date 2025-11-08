61dc9d3bf0ef40a8a9327a3e10746ef93f8a81d0
Tin Trinh <tintct99@gmail.com>
2025-11-08 23:44:40 +0700

    new file added

---
+++ cloud-run/test/main.py
@@ -0,0 +1,13 @@
+from google.cloud import bigquery
+# from google.oauth2 import service_account
+
+# key_path = "gcp-sa-001.json"
+# credentials = service_account.Credentials.from_service_account_file(key_path)
+client = bigquery.Client(project="gcp-owner-self-learning")
+
+query = """
+    insert into gcp-owner-self-learning.pubsub_dataflow_uat.pubsub_dataflow_bq_v1
+    values(12,'tin12')
+"""
+
+df = client.query(query)
