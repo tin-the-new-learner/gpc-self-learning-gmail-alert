02d8e98b73ed15bff62951e78ccef75f24b98b83
Tin Trinh <tintct99@gmail.com>
2025-11-18 02:39:15 +0700

    test new added

--- cloud-run/test/main.py
+++ cloud-run/test/main.py
@@ -4,7 +4,6 @@ from google.cloud import bigquery
 # key_path = "gcp-sa-001.json"
 # credentials = service_account.Credentials.from_service_account_file(key_path)
 client = bigquery.Client(project="gcp-owner-self-learning")
-
 query = """
     insert into gcp-owner-self-learning.pubsub_dataflow_uat.pubsub_dataflow_bq_v1
     values(14,'tin14')
